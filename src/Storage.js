37af30f018cba6f230ebe811eb3479440f278f23
ralbertazzi <piano.vergato@gmail.com>
2018-12-10 20:59:31 +0100

    Added storage functionalities

---
+++ src/Storage.js
@@ -0,0 +1,11 @@
+import { AsyncStorage } from "react-native"
+
+
+export async function storeData(key, data) {
+    await AsyncStorage.setItem(key, JSON.stringify(data))
+}
+
+export async function retrieveData(key) {
+    let data = await AsyncStorage.getItem(key)
+    return JSON.parse(data)
+}
