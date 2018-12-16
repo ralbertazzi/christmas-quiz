821b8c0ee072d2a7591b972e9d7b96a2eb2e38f5
ralbertazzi <piano.vergato@gmail.com>
2018-12-16 11:11:09 +0100

    Using text component for long texts

--- src/Levels.js
+++ src/Levels.js
@@ -30,14 +30,15 @@ levels = [
     {
         tag: SimpleCard,
         title: "Alla ricerca dei regali perduti",
-        text: "Anche quest'anno Babbo Natale è pronto per portare i suoi regali in giro per il mondo 🎁🎁🎁." +
-                "Il caro vecchio uomo barbuto si è però accorto di quanto tu sia stata una ragazza cattiva quest'anno. " +
-                "Nonostante ciò, non ha deciso di non portarti doni (stiamo parlando di Babbo Natale d'altronde!), " +
-                "ma ha deciso che per ottenere i suoi regali dovrai sudartela parecchio . In questo modo, la sua coscienza è a posto " + 
-                "e il fatto che tu riceva o meno i tanti attesi regali dipendono soltanto dalla tua arguzia e forza di volontà.\n" +
-                "Durante le sue notti solitarie nell'incontaminata Lapponia, il nostro Santa Claus si è inventato una serie di " +
-                "intricati indovinelli che, solo una volta risolti tutti, ti porteranno ai tanto desiderati regali. " +
-                "Avanti! Raduna i tuoi migliori pesanti vestiti natalizi, cuffia e sciarpa e preparati per questa avventura",
+        text: <Text>Anche quest'anno Babbo Natale è pronto per portare i suoi regali in giro per il mondo 🎁🎁🎁.
+                Il caro vecchio uomo barbuto si è però accorto di quanto tu sia stata una ragazza cattiva quest'anno.
+                Nonostante ciò, non ha deciso di non portarti doni (stiamo parlando di Babbo Natale d'altronde!),
+                ma ha deciso che per ottenere i suoi regali dovrai sudartela parecchio. In questo modo, la sua coscienza è a posto 
+                e il fatto che tu riceva o meno i tanti attesi regali dipendono soltanto dalla tua arguzia e forza di volontà.<NewLine/>
+                Durante le sue notti solitarie nell'incontaminata Lapponia, il nostro Santa Claus si è inventato una serie di
+                intricati indovinelli che, solo una volta risolti tutti, ti porteranno ai tanto desiderati regali.
+                Avanti! Raduna i tuoi migliori pesanti vestiti natalizi, cuffia e sciarpa e preparati per questa avventura!
+                </Text>,
         image: require('../assets/babbo-natale-renne.jpg'),
         buttonText: "Ma cosa mi tocca fare"
     },
@@ -50,32 +51,35 @@ levels = [
     {
         tag: SimpleCard,
         title: "Come funziona il gioco",
-        text: "Le regole sono molto semplici! Gli indovinelli ti saranno posti uno dopo l'altro, e una volta risolto uno passerai a quello successivo." +
-                "Per tenere traccia dello stato della tua disperata ricerca, potrai sempre visualizzare la barra in alto che " +
-                "segnala la percentuale di completamento del gioco. Fin qui tutto semplice no?\n" +
-                "Siccome Babbo Natale è buono, ha concesso che in momenti di difficoltà tu possa chiedere un aiuto. " +
-                "La richiesta di aiuto può essere di ogni tipo, dal non capire un indovinello, dal non riuscire ad arrivare alla soluzione, " +
-                "a (soprattutto, ma non succederà) bug nell'applicazione che ti impediscano di proseguire nel gioco. Sarebbe un vero peccato!" + 
-                "Ecco quindi che, una volta chiesto aiuto (e a chi se non al tuo meraviglioso fidanzato?), riceverai in risposta un codice che ti " +
-                "permetterà di sbloccare indizi o addirittura passare all'indovinello successivo. Dove inserire questo codice? Molto semplice! " + 
-                "Clicca sull'icona in alto a sinistra e inserisci il codice! (Ricorda che maiuscole e minuscole contano).\n\n" +
-                "Per passare alla prossima schermata, clicca sull'icona in alto a destra e inserisci il codice 'riccardo'",
+        text: <Text>Le regole sono molto semplici! Gli indovinelli ti saranno posti uno dopo l'altro, e una volta risolto uno passerai a quello successivo.
+                Per tenere traccia dello stato della tua disperata ricerca, potrai sempre visualizzare la barra in alto che
+                segnala la percentuale di completamento del gioco. Fin qui tutto semplice no?<NewLine/>
+                Siccome Babbo Natale è buono, ha concesso che in momenti di difficoltà tu possa chiedere un aiuto.
+                La richiesta di aiuto può essere di ogni tipo, dal non capire un indovinello, dal non riuscire ad arrivare alla soluzione,
+                a (soprattutto, ma non succederà) bug nell'applicazione che ti impediscano di proseguire nel gioco. Sarebbe un vero peccato! 
+                Ecco quindi che, una volta chiesto aiuto (e a chi se non al tuo meraviglioso fidanzato?), riceverai in risposta un codice che ti
+                permetterà di sbloccare indizi o addirittura passare all'indovinello successivo. Dove inserire questo codice? Molto semplice! 
+                Clicca sull'icona in alto a sinistra e inserisci il codice! (Ricorda che maiuscole e minuscole contano).<NewLine/>
+                Per passare alla prossima schermata, clicca sull'icona in alto a destra e inserisci il codice 'riccardo'
+                </Text>,
         image: require('../assets/snoopy-detective.jpg'),
         endHint: "riccardo"
     },
     {
         tag: RequestPermissions,
-        text: "Come dicono a Città del Capo, we are all set! Rimane soltanto un passaggio: " +
-                "accettare i permessi di accedere alla tua posizione GPS e cominciare a giocare!" +
-                "In bocca al lupo e May the Force be with you!",
+        text: <Text>Come dicono a Città del Capo, we are all set! Rimane soltanto un passaggio:
+                accettare i permessi di accedere alla tua posizione GPS e cominciare a giocare!
+                In bocca al lupo e May the Force be with you!
+                </Text>,
         title:'Require permissions',
         buttonText:'Grant permissions'
     },
     {
         tag: GpsComponent,
         title: "Comincia l'avventura",
-        text: "Sembra che Babbo Natale sia già stato avvistato in zona! Poco fa la sua slitta era parcheggiata a Pontecchio Marconi. " + 
-                "Dirigiti là e comincia a investigare",
+        text: <Text>Sembra che Babbo Natale sia già stato avvistato in zona! Poco fa la sua slitta era parcheggiata a Pontecchio Marconi. 
+                    Dirigiti là e comincia a investigare
+                </Text>,
         image: require('../assets/marconi.jpg'),
         location: GPS_MARCONI,
         gpsHint: "marconi",
